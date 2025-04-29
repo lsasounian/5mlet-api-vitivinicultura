c315be42fde90d234403a8bc0f996e3e390f7b48
Tiago Ribeiro <tiagoparibeiro@gmail.com>
2025-04-29 17:35:55 -0300

    fix: adicionando handler

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from app.routers import login, producao, processamento, comercializacao, importacao, exportacao, classificador_abc
+from mangum import Mangum
 
 app = FastAPI(title="Vitivinicultura API - Embrapa")
 
 @app.get("/")
 async def root():
     return {"greeting": "Olá, bem vindo a API de Vitivinicultura da Embrapa", "message": "Dúvidas? Faça uma requisição /docs e veja a documentação completa"}
-    
+
+handler = Mangum(app)
+
 app.include_router(login.router)
 app.include_router(producao.router)
 app.include_router(processamento.router)
