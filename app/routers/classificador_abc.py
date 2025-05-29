301cb7aab1b2906bb8314f789780b6d36bdee7e9
Samuel Guedes de Melo <samuelguedes.net@gmail.com>
2025-05-29 20:07:07 -0300

    Correção de subopcao no insomnia e bug no cassificador ABC

--- app/routers/classificador_abc.py
+++ app/routers/classificador_abc.py
@@ -10,7 +10,7 @@ router = APIRouter(
 )
 
 @router.get(
-    " ",
+    "",
     summary="Este endpoint gera uma curva ABC dos dados de exportação de vinhos e derivados, já devidamente classificada, nos mostrando uma pespectiva dos maiores clientes desse tipo de produto.",
     description="Esta funcionalidade recebe o retorndo em formato json do endpoint de exportação, repassa o atributo dados desse retorno como parâmetro.",
     response_description="Retorna uma curva ABC de dados de exportação de vinhos e derivados."
