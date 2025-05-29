be04b48ca6aab0817a7fd3af81b4df69fc46ce4d
Samuel Guedes de Melo <samuelguedes.net@gmail.com>
2025-05-29 12:39:02 -0300

    Correção da documentação indicando como utilizar a curva ABC da exportação

--- app/routers/classificador_abc.py
+++ app/routers/classificador_abc.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, Query
+
 from app.services.auth import verify_token
 from app.services.classificador_abc import exportacao_abc
 
@@ -9,9 +10,11 @@ router = APIRouter(
 )
 
 @router.get(
-    "",
-    summary="Classificador ABC",
-    description="Retorna os dados de Classificador ABC"
+    " ",
+    summary="Este endpoint gera uma curva ABC dos dados de exportação de vinhos e derivados, já devidamente classificada, nos mostrando uma pespectiva dos maiores clientes desse tipo de produto.",
+    description="Esta funcionalidade recebe o retorndo em formato json do endpoint de exportação, repassa o atributo dados desse retorno como parâmetro.",
+    response_description="Retorna uma curva ABC de dados de exportação de vinhos e derivados."
+    
 )
 
 def get_abc_exportacao(dados: str = Query(None)):
